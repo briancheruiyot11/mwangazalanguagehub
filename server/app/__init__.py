ac7a3f63d240436e7d7e0bb51517c74a386176b8
Brian Cheruiyot <briancheru11@gmail.com>
2026-03-27 17:13:13 +0300

    Use create_all instead of migrations

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, app, jsonify
 from flask_cors import CORS
 
 from config import Config
@@ -40,4 +40,7 @@ def create_app():
     def home():
         return jsonify({"message": "Server is running"})
 
+    with app.app_context():
+        db.create_all()
+    
     return app
